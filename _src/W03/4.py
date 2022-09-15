6a628995553809f4de79fc4242cc78fe59ffcf33
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-09-15 12:40:30 +0900

    feat: wordCloud
    import data, tokenize, normalize, remove stopWords, matplotLib, wordColud

--- _src/W03/4.py
+++ _src/W03/4.py
@@ -1,27 +1,87 @@
-import nltk
-import ssl
 from nltk.corpus import gutenberg
-
-## Set SSL Setting
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    print("err")
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-# Code
-
-#nltk.download('gutenberg') # 다운로드
-file_names = gutenberg.fileids() # 제공하는 파일명을 읽어와서 출력
-print(file_names)
-
-
-
 from nltk.tokenize import word_tokenize
-nltk.download('punkt')
-doc_alice = gutenberg.open('carroll-alice.txt').read()
-print('Length of document:', len(doc_alice))
-tokens_alice = word_tokenize(doc_alice) # word_tokenize()로 토큰화 실행
-print('Num of tokens used:', len(tokens_alice))
+
+print("========Tokenize=========")
+# alice.txt 객체 선언
+alice = gutenberg.open('carroll-alice.txt').read()
+# Tokenize
+tokens_alice = word_tokenize(alice) # word_tokenize()로 토큰화 실행
+
+######################################################
+print("========Nomarlization : Stem =========")
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+# 삼항 연산자 내부의 for문 실행. stemmer애 존재하는 어간 추출을 통한 정규화.
+stemmer_token_alice = [stemmer.stem(i) for i in tokens_alice]
+
+print("======== Nomarlization : WordnetLemmatizer =========")
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+lem_tokens_alice = [lemmatizer.lemmatize(token) for token in tokens_alice]
+
+print("======== Nomarlization : Regex =========")
+from nltk.tokenize import RegexpTokenizer
+# 세 문자 이상의 단어만 추출
+tokenizer = RegexpTokenizer("[\w']{3,}")
+# 소문자 변환 후 토큰화 실행
+reg_tokens_alice = tokenizer.tokenize(alice.lower())
+
+######################################################
+
+#각각 정규화 한 Data들을 List에 넣어줌.
+normalized_data = [stemmer_token_alice, lem_tokens_alice, reg_tokens_alice]
+# map이 Object를 return해서 일단 pass
+
+print("======== 불용어 제거 =========")
+from nltk.corpus import stopwords
+english_stops = set(stopwords.words('english'))
+
+
+# 불용어처리 callBack 함수 (정규화 데이터)=> 불용어 처리 데이터
+def return_stopwords(normalized_data):
+    return [i for i in normalized_data if i not in english_stops]
+
+# map을 사용해 normalizedData를 받아 불용어 처리가 끝난 data return.
+# newIterable = map(callbackFunc, iterable)
+
+result_alice = return_stopwords(reg_tokens_alice)
+
+
+print("======== 단어 빈도별 계산 =========")
+stemmer_alice_word_count = dict() #안씀
+lem_alice_word_count = dict() #안씀
+regex_alice_word_count = dict()
+
+for word in result_alice: # 모든 토큰에 대해
+    regex_alice_word_count[word] = regex_alice_word_count.get(word, 0) + 1
+    sorted_word_count = sorted(regex_alice_word_count, key=regex_alice_word_count.get, \
+reverse=True)
+
+#print(regex_alice_word_count)
+
+
+print("======== 빈도 순위 확인 =========")
+for key in sorted_word_count[:20]: #빈도수 상위 20개의 단어를 출력
+    print(f'{repr(key)}: {regex_alice_word_count[key]}', end=', ')
+
+print("======== MatplotLib =========")
+import matplotlib.pyplot as plt
+# 빈도수 상위 20개의 단어를 추출해 역순으로 정렬
+n = sorted_word_count[:20][::-1]
+# 20개 단어에 대한 빈도
+w = [regex_alice_word_count[key] for key in n]
+# 수평 막대그래프
+plt.barh(range(len(n)),w,tick_label=n)
+
+# plt.show() # Window Rendering
+
+# WordCount 실행 예제
+
+from wordcloud import WordCloud
+# 워드 클라우드 이미지 생성
+wordcloud = WordCloud()
+wordcloud.generate(alice)
+plt.axis("off") # 축이 보이지 않게 설정
+plt.imshow(wordcloud) #plt.imshow()로 이미지 출력
+plt.show()
